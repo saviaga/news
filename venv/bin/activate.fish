6d30b0027a346344f574552ba4a65d36b12b437e
saviaga <saviaga@hotmail.com>
2016-04-25 20:42:06 -0500

    make it better

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/claudia/PycharmProjects/FlaskRetweet/venv"
+set -gx VIRTUAL_ENV "/home/claudia/PycharmProjects/FlaskRetweet/src/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
